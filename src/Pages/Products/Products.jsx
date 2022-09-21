08c514ed1693327c81da02929abd9f99a8994188
Joao Coelho <jancoweb@outlook.com>
2022-09-21 20:09:07 -0300

    item container

--- src/Pages/Products/Products.jsx
+++ src/Pages/Products/Products.jsx
@@ -1,20 +1,51 @@
 import Header from "../../Components/Header";
 import Footer from "../../Components/Footer";
 import './productStyle.css';
+import { useEffect, useState } from "react";
+import bolos from "../../Components/ProductsArr/bolos";
 
 export default function ProductsPage() {
+
+  const [active, setActive] = useState('');
+  const [produtos, setProdutos] = useState([])
+
+  function handleSelection(e) {
+    setActive('');
+    setActive(e.target.innerHTML.toLowerCase());
+
+  }
+  useEffect(() => {
+    if (active == 'bolos') return setProdutos(bolos)
+    return setProdutos([])
+  }, [active])
+
 
   return (
     <div>
       <Header active='products' />
       <nav>
         <ul className="product_nav_list">
-          <li>Bolos</li>
-          <li>Tortas</li>
-          <li>Vulcões</li>
-          <li>Taça de bolo</li>
+          <li onClick={(e) => handleSelection(e)} className={active == 'bolos' ? 'active' : null}>Bolos</li>
+          <li onClick={(e) => handleSelection(e)} className={active == 'tortas' ? 'active' : null}>Tortas</li>
+          <li onClick={(e) => handleSelection(e)} className={active == 'vulcões' ? 'active' : null}>Vulcões</li>
+          <li onClick={(e) => handleSelection(e)} className={active == 'taça de bolo' ? 'active' : null}>Taça de bolo</li>
         </ul>
       </nav>
+      {
+        produtos && produtos.map((produto) => {
+          return (
+            <div className="item" key={produto.id}>
+              <div className="item_img_container">
+                <img src={produto.img} alt="" />
+              </div>
+              <div className="item_content_container">
+                <p>{produto.nome}</p>
+                <p>Saiba mais</p>
+              </div>
+            </div>
+          )
+        })
+      }
       <Footer />
     </div>
   )
