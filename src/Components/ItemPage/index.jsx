7e5aae0416aea85d75b4eb97606fe814f8885b49
jancoweb <jancoweb@outlook.com>
2022-10-03 09:51:14 -0300

    item page

--- src/Components/ItemPage/index.jsx
+++ src/Components/ItemPage/index.jsx
@@ -1,14 +1,39 @@
+import ContactContainer from "../ContactContainer";
 import Footer from "../Footer";
 import { useGlobalContext } from "../GlobalContext/useContext";
 import Header from "../Header";
 import "./itemStyle.css";
 
 export default function ItemPage() {
-  const { itemPage, setItemPage } = useGlobalContext();
-
+  const { itemPage } = useGlobalContext();
+  console.log(itemPage)
   return (
     <>
       <Header />
+      <div className="item_page_container">
+        <div className="item_page_head_content">
+          <div className="item_page_head_img">
+            <img src={itemPage.img} alt="" />
+          </div>
+          <div className="item_page_head_text">
+            <h2>{itemPage.nome}</h2>
+            <p className="mid">O sucesso da Villa!</p>
+            <p className="small">{itemPage.description}</p>
+          </div>
+        </div>
+        <div className="item_page_mid_container">
+          <div className="item_page_mid_content">
+            <h3>Descrição do produto</h3>
+            <p className="small">
+              Etiam condimentum orci eget tortor pharetra dignissim. Donec at diam non arcu aliquam feugiat. Cras dapibus commodo purus eu varius. Nullam commodo faucibus nulla in sodales. Suspendisse potenti. Integer viverra vestibulum sagittis. Ut lacus diam, sagittis a suscipit eget, lobortis quis dui.
+            </p>
+          </div>
+          <div>
+            <img src={itemPage.img} alt="" />
+          </div>
+        </div>
+      </div>
+      <ContactContainer />
 
       <Footer />
     </>
--- src/Pages/Products/Products.jsx
+++ src/Pages/Products/Products.jsx
@@ -1,86 +1,100 @@
 import Header from "../../Components/Header";
 import Footer from "../../Components/Footer";
-import './productStyle.css';
+import "./productStyle.css";
 import { useEffect, useState } from "react";
-import arrowIcon from '../../../assets/arrow_forward.png';
+import arrowIcon from "../../../assets/arrow_forward.png";
 import ContactContainer from "../../Components/ContactContainer";
 import api from "../../services/api";
 import { useGlobalContext } from "../../Components/GlobalContext/useContext";
 import { useNavigate } from "react-router-dom";
 
 export default function ProductsPage() {
-
   const navigate = useNavigate();
   const { active, setActive } = useGlobalContext();
-  const { itemPage, setItemPage } = useGlobalContext();
+  const { setItemPage } = useGlobalContext();
   const [produtos, setProdutos] = useState([]);
 
   function handleSelection(e) {
-    setActive('');
+    setActive("");
     setActive(e.target.innerHTML.toLowerCase());
-
   }
 
-  async function handleItem(id){
+  async function handleItem(id) {
     try {
       const res = await api.get(`/produto/${id}`);
       setItemPage(res.data);
       return navigate(`/produtos/${id}`);
-    } catch(error){
-      return error.message
+    } catch (error) {
+      return error.message;
     }
   }
 
   useEffect(() => {
     async function handlePopulate() {
       let id = 0;
-      if (active == 'bolos') id = 1;
-      if (active == 'tortas') id = 2;
-      if (active == 'vulcões') id = 3;
-      if (active == 'taça de bolo') id = 4;
+      if (active == "bolos") id = 1;
+      if (active == "tortas") id = 2;
+      if (active == "vulcões") id = 3;
+      if (active == "taça de bolo") id = 4;
       try {
         const res = await api.get(`/produto/tipo/${id}`);
-        setProdutos(res.data)
+        setProdutos(res.data);
       } catch (error) {
-        return error.message
+        return error.message;
       }
     }
-    handlePopulate()
+    handlePopulate();
   }, [active]);
-
 
   return (
     <>
-      <Header active='products' />
+      <Header active="products" />
       <div className="products_container">
         <nav>
           <ul className="product_nav_list">
-            <li onClick={(e) => handleSelection(e)} className={active == 'bolos' ? 'active' : null}>Bolos</li>
-            <li onClick={(e) => handleSelection(e)} className={active == 'tortas' ? 'active' : null}>Tortas</li>
-            <li onClick={(e) => handleSelection(e)} className={active == 'vulcões' ? 'active' : null}>Vulcões</li>
-            <li onClick={(e) => handleSelection(e)} className={active == 'taça de bolo' ? 'active' : null}>Taça de bolo</li>
+            <li
+              onClick={(e) => handleSelection(e)}
+              className={active == "bolos" ? "active" : null}>
+              Bolos
+            </li>
+            <li
+              onClick={(e) => handleSelection(e)}
+              className={active == "tortas" ? "active" : null}>
+              Tortas
+            </li>
+            <li
+              onClick={(e) => handleSelection(e)}
+              className={active == "vulcões" ? "active" : null}>
+              Vulcões
+            </li>
+            <li
+              onClick={(e) => handleSelection(e)}
+              className={active == "taça de bolo" ? "active" : null}>
+              Taça de bolo
+            </li>
           </ul>
         </nav>
         <div className="item_list_container">
-          {
-            produtos && produtos.map((produto) => {
+          {produtos &&
+            produtos.map((produto) => {
               return (
-                <div className="item" key={produto.id} onClick={()=>{navigate(`/produtos/${produto.id}`)}}>
+                <div className="item" key={produto.id} onClick={() => { handleItem(produto.id); }}>
                   <div className="product_item_img_container">
                     <img src={produto.img} alt="" />
                   </div>
                   <div className="product_item_content_container">
                     <h3 className="item_name">{produto.nome}</h3>
-                    <p className="mid">Saiba mais <img src={arrowIcon} alt="" /></p>
+                    <p className="mid">
+                      Saiba mais <img src={arrowIcon} alt="" />
+                    </p>
                   </div>
                 </div>
-              )
-            })
-          }
+              );
+            })}
         </div>
         <ContactContainer />
       </div>
       <Footer />
     </>
-  )
+  );
 }
