f8ba6501285a794800058748567e29091e1c56ff
jancoweb <jancoweb@outlook.com>
2022-10-03 10:09:51 -0300

    hotfix

--- src/Components/ItemPage/index.jsx
+++ src/Components/ItemPage/index.jsx
@@ -6,6 +6,7 @@ import ItemNav from "../ItemNav";
 import Arrow_back from '../../../assets/arrow_back.png'
 import "./itemStyle.css";
 import { useNavigate } from "react-router-dom";
+import Hashtag from '../../../assets/hash.png'
 
 export default function ItemPage() {
   const navigate = useNavigate()
@@ -29,7 +30,7 @@ export default function ItemPage() {
         </div>
         <div className="item_page_mid_container">
           <div className="item_page_mid_content">
-            <h3>Descrição do produto</h3>
+            <h3><img src={Hashtag} style={{ width: '40px' }} alt="" /> Descrição do produto</h3>
             <p className="small">
               Etiam condimentum orci eget tortor pharetra dignissim. Donec at diam non arcu aliquam feugiat. Cras dapibus commodo purus eu varius. Nullam commodo faucibus nulla in sodales. Suspendisse potenti. Integer viverra vestibulum sagittis. Ut lacus diam, sagittis a suscipit eget, lobortis quis dui.
             </p>
